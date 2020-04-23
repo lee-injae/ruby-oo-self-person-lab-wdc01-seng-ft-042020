8cd34d534526d6f2c3214aec13734ce1e907a487
mertoz41 <mertozkaynak@windowslive.com>
2020-04-22 22:31:49 -0400

    fourth commit

--- lib/person.rb
+++ lib/person.rb
@@ -13,50 +13,44 @@ class Person
         #getter  
     end 
 
-    # def name= (new_name)
-    #     @name = new_name 
-    #     #setter 
-    # end
+     def bank_account
+         @bank_account
+    
+     end
 
-    # def bank_account (amount)
-    #     @bank_account = amount 
-    #     #setter
-    # end
-
-    def personal_account
-        @bank_account
-        #getter
+    def bank_account= (number)
+       @bank_account = @bank_account + number
     end 
 
-#     def happiness(points= 8)
-#         if points > 10 
-#             points = 10 
-#         elsif points < 0 
-#             points = 0 
-#         end 
-#         @happiness = points 
-#         #setter
-#     end
 
-#     def personal_happiness
-#         @happiness 
-#         #getter
-#     end 
+     def happiness
+        @happiness 
+     end
 
-#     def hygiene(points)
-#         if points > 10 
-#             points = 10 
-#         elsif points < 0 
-#             points = 0 
-#         end 
-#         @hygiene = points 
-#         #setter
-#     end
+     def happiness= (number)
+       
+        if @happiness + number >= 10 
+           @happiness 
+        elsif 
+           @happiness + number <= 0
+            @happiness 
+        end 
 
-#     def personal_hygine
-#         @hygiene 
-#         #getter
-#     end 
+        
+     end 
+
+     def hygiene
+        @hygiene
+     end 
+
+     def hygiene= (number)
+
+
+     end 
+
+
+
+
 
 
 
@@ -72,17 +66,18 @@ class Person
 #             return false 
 #     end
 
-#     def happy?
-#         if @happiness > 7
-#             return true 
-#         else 
-#             return false
-#     end
+    #  def happy?
+    #     if @happiness > 7
+    #         return true 
+    #     else 
+    #         return false
+    #     end 
+    # end
 
-#     def get_paid(salary_amount)
-#     @bank_account = bank_account + salary_amount
-#     return "all about benjamins"
-#     end
+    def get_paid(salary)
+    @bank_account = @bank_account + salary
+    pp "all about the benjamins"
+    end
 
 #     def take_bath
 #         @hygine = @hygine + 4  
